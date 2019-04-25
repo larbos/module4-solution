96ed19daccdd983b85677e24deaa9d87b5eac29e
larbos <47331339+larbos@users.noreply.github.com>
2019-04-25 17:46:26 -0300

    index e script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-var x = "Hello";
-x += " World"
-console.log(x + "!")
+var x;
+console.log(x);
+
+if (x == true) {
+	console.log("x is undefined");
+}
+
+x = 5;
+if (x == undefined) {
+	console.log("x is undefined");
+}
+else {
+	console.log("x has been defined");
+}
